ee2be65c48158f2adb97141458e877a87818a721
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-03 02:30:07 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(
+    process.env.MONGO_URI
+  )
+  .then(() => {
+    console.log("MongoDB is Connected");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000!!");
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!!");
+});
