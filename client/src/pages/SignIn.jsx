7679f8ec9c2db4f324222f1467ee642e53e6d7a2
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-11 23:29:18 +0530

    Complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,89 @@
-
-
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
 function SignIn() {
+    const [formData, setFormData] = useState({});
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!formData.email || !formData.password) {
+            return setErrorMessage('please fill out all fields.');
+        }
+        try {
+            setLoading(true);
+            setErrorMessage(null);
+            const res = await fetch('/api/auth/signin', {
+                method: "POST",
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(formData),
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                return setErrorMessage(data.message);
+            }
+            setLoading(false);
+            if(res.ok){
+                navigate('/');
+            }
+        } catch (error) {
+            setErrorMessage(error.message);
+            setLoading(false);
+        }
+    }
     return (
-        <div>
-SignIn
+        <div className="min-h-screen mt-20">
+            <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+                {/* left side */}
+                <div className="flex-1">
+                    <Link to="/" className=" font-bold dark:text-white text-4xl">
+                        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+                            {"Prince's"}
+                        </span>
+                        Blog
+                    </Link>
+                    <p className="text-sm mt-5">
+                        This is demo project. You can sign in with your email and password
+                        or with Google.
+                    </p>
+                </div>
+                {/* right side */}
+                <div className="flex-1">
+                    <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+                        <div>
+                            <Label value="Email"></Label>
+                            <TextInput type="email" placeholder="example@email.com" id="email" onChange={handleChange}></TextInput>
+                        </div>
+                        <div>
+                            <Label value="Password"></Label>
+                            <TextInput type="password" placeholder="**********" id="password" onChange={handleChange}></TextInput>
+                        </div>
+                        <Button gradientDuoTone='purpleToPink' type="submit" disabled={loading}>{loading ? (
+                            <>
+                                <Spinner size='sm' />
+                                <span className="pl-3">Loading...</span>
+                            </>
+                        ) : 'Sign In'}</Button>
+                    </form>
+                    <div className="flex gap-2 text-sm mt-5">
+                        <span>{"Don't have an account?"}</span>
+                        <Link to='/sign-up' className="text-blue-500">Sign up</Link>
+                    </div>
+                    {
+                        errorMessage && (
+                            <Alert className="mt-5" color='failure'>
+                                {errorMessage}
+                            </Alert>
+                        )
+                    }
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default SignIn
+export default SignIn;
