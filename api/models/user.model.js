828ba0288f8b94963b2242f6582cbd3e1938730d
Prince Bhayani <princebhayani192003@gmail.com>
2024-04-23 22:19:24 +0530

    complete google OAuth functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -14,6 +14,11 @@ const userSchema = mongoose.Schema({
     password:{
         type:String,
         require:true
+    }, 
+    profilePicture: {
+        type: String,
+        default:
+          'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
 },{timestamps:true});
 
