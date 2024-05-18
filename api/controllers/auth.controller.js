a16719d85af347b2842870fc9c8ce732a0e3526a
Prince Bhayani <princebhayani192003@gmail.com>
2024-05-19 03:47:01 +0530

    add admin functionality to the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -49,7 +49,10 @@ export const signin = async (req, res, next) => {
     if (!validPassword) {
        return next(errorHandler(400, "Invalid credentials"));
     }
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
     const {password : pass, ...rest} = validUser._doc;
     res.status(200).cookie("access_token",token,{
       httpOnly:true
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -20,6 +20,10 @@ const userSchema = mongoose.Schema({
         default:
           'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 },{timestamps:true});
 
 const User = mongoose.model("User",userSchema);
