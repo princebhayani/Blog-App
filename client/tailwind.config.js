094dcae499cf7ed69d8017332b9c7a94889eb430
princebhayani1902 <princebhayani192003@gmail.com>
2024-06-06 16:12:27 +0530

    add recent article section to the post page

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,5 +9,8 @@ export default {
     extend: {},
   },
   // eslint-disable-next-line no-undef
-  plugins: [require("flowbite/plugin")],
+  plugins: [
+    require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
+  ],
 }
