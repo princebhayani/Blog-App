d0a4ff7489a72fe159c97b0c7e005fa7439b8c8a
princebhayani1902 <princebhayani192003@gmail.com>
2024-06-14 19:01:07 +0530

    complete about and projects pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -2,8 +2,10 @@
 
 function Projects() {
     return (
-        <div>
-
+        <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+            <h1 className='text-3xl font-semibold'>Pojects</h1>
+            <p className='text-md text-gray-500'>Build fun and engaging projects while learning HTML, CSS, and JavaScript!</p>
+            <CallToAction />
         </div>
     )
 }
