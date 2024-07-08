9d0c6df04c510a12f297631086d5f617091ef974
princebhayani1902 <princebhayani192003@gmail.com>
2024-07-08 18:44:29 +0530

    update link

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,14 +5,14 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about JavaScript?
+                Want to learn more ?
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with 100 JavaScript Projects
+                Checkout these resources with JavaScript Projects
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
-                <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                    100 JavaScript Projects
+                <a href="https://github.com/princebhayani" target='_blank' rel='noopener noreferrer'>
+                    JavaScript Projects
                 </a>
             </Button>
         </div>
